0c02cf05592c863fceb22b2a269c9741f02e3828
STIg89 <andreystig89@gmail.com>
2023-04-17 22:21:55 +0300

    done

---
+++ index.js
@@ -0,0 +1,44 @@
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts");
+const { Command } = require("commander");
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contactList = await listContacts();
+      return console.table(contactList);
+
+    case "get":
+      const receivedContact = await getContactById(id);
+      return console.log(receivedContact);
+
+    case "add":
+      const addedContact = await addContact(name, email, phone);
+      return console.log(addedContact);
+
+    case "remove":
+      const removedContact = await removeContact(id);
+      return console.log(removedContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
